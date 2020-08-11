35a1f042d18207590e138e018e2a8df06dc46c73
emmattfu <makcimdjan@gmail.com>
2020-08-11 14:59:31 +0300

    some

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -6,6 +6,8 @@ import {
   InfoWindow,
 } from "@react-google-maps/api";
 
+import icon from "../assets/school.png";
+
 const mapContainerStyle = {
   width: "100%",
   height: "500px",
@@ -40,6 +42,12 @@ const MyGoogleMap = () => {
       >
         <Marker
           position={center}
+          icon={{
+            url: icon,
+            scaledSize: new window.google.maps.Size(30, 30),
+            origin: new window.google.maps.Point(0, 0),
+            anchor: new window.google.maps.Point(15, 15)
+          }}
           onClick={() => setSelected(true)}
         />
       </GoogleMap>
