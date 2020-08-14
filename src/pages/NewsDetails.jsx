34a7c810392e3ba611505873cadd8a208a804d3b
emmattfu <makcimdjan@gmail.com>
2020-08-14 14:46:23 +0300

    youtube

--- src/pages/NewsDetails.jsx
+++ src/pages/NewsDetails.jsx
@@ -1,32 +1,43 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import {useDispatch} from 'react-redux'
+import { useDispatch } from "react-redux";
 import { getSelectedNews } from "../redux/actions/news";
+import YouTube  from 'react-youtube'
+import "../styles/detailedNews.css";
 
 const NewsDetails = ({ match }) => {
   const id = match.params.id;
 
-  // const selectedNews = useSelector((state) =>
-  //   state.news.news.filter((el) => el.id === id) || state.news.selectedNews
-  // );
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   React.useEffect(() => {
-    dispatch(getSelectedNews(id))
-  }, [])
+    dispatch(getSelectedNews(id));
+  }, []);
 
-  const selectedNews = useSelector(state => state.news.selectedNews)
-  console.log(selectedNews)
+  const selectedNews = useSelector((state) => state.news.selectedNews);
+  console.log(selectedNews);
 
   return (
     <>
       {selectedNews.title && (
         <div className="detailed-news page-block">
-          <h3 className="page-title">{selectedNews.title}</h3>
-          <img src={selectedNews.url} alt="card-img" style={{width: '50%'}}/>
+          <div className="detailed-news__header">
+            <h3 className="detailed-news__title page-title">
+              {selectedNews.title}
+            </h3>
+            <span className="detailed-news__time">
+              {new Date(selectedNews.createdAt.seconds * 1000).toLocaleString()}
+            </span>
+          </div>
+          <img src={selectedNews.url} alt="card-img" style={{ width: "75%" }} />
           <p>{selectedNews.text}</p>
-          <p>Більше фото на <a href={selectedNews.morePhoto}>нашій сторінці в Фейсбук</a></p>
+
+          <YouTube videoId="2g811Eo7K8U"  />
+
+          <p>
+            Більше фото на{" "}
+            <a href={selectedNews.morePhoto}>нашій сторінці в Фейсбук</a>
+          </p>
         </div>
       )}
     </>
